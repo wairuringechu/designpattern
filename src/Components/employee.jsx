8b51b9d5427e5c6d10ba8d8197a6fbb5b3186c73
Wairuri <ngechmarion@gmail.com>
2023-02-24 12:54:44 +0300

    designpattern

---
+++ src/Components/employee.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const employee = () => {
+    constructor(name, age, employeeId) 
+         super(name, age);
+        this.employeeId = employeeId;
+  return (
+    <div class ="employee">
+        <employeeId/>
+    </div>
+  )
+}
+export default employee;
---
+++ src/Components/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const person = () =>{
+    constructor(name, age)
+        this.name = name;
+         this.age = age;
+}
+  return(
+    <div class ="person"> 
+        <age/>
+        <name/>
+    <div/>
+    )
+
+Export default person
